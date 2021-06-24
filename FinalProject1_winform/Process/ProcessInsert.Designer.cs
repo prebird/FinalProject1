0285c720795019f04bfce3fc28bd306ed4bcaa5b
박기덕 <lnaremay@gmail.com>
2021-06-24 17:40:03 +0900

    ..

--- FinalProject1_winform/Process/ProcessInsert.Designer.cs
+++ FinalProject1_winform/Process/ProcessInsert.Designer.cs
@@ -34,8 +34,15 @@ namespace FinalProject1_winform.Process
             this.pictureBox_gudi1 = new FinalProject1_winform.PictureBox_gudi();
             this.label_gudi1 = new FinalProject1_winform.Label_gudi();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button_gudi2 = new FinalProject1_winform.Button_gudi();
+            this.button_gudi1 = new FinalProject1_winform.Button_gudi();
+            this.label_gudi2 = new FinalProject1_winform.Label_gudi();
+            this.label_gudi3 = new FinalProject1_winform.Label_gudi();
+            this.label_gudi4 = new FinalProject1_winform.Label_gudi();
+            this.placeholderNew1 = new FinalProject1_winform.PlaceholderNew();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_gudi1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -72,11 +79,88 @@ namespace FinalProject1_winform.Process
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.button_gudi2);
+            this.panel2.Controls.Add(this.button_gudi1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 319);
+            this.panel2.Location = new System.Drawing.Point(0, 321);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(474, 67);
+            this.panel2.Size = new System.Drawing.Size(474, 65);
             this.panel2.TabIndex = 3;
+            // 
+            // button_gudi2
+            // 
+            this.button_gudi2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_gudi2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(146)))), ((int)(((byte)(209)))));
+            this.button_gudi2.ButtonType = FinalProject1_winform.ButtonStyle.Highlight;
+            this.button_gudi2.FlatAppearance.BorderSize = 0;
+            this.button_gudi2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_gudi2.Font = new System.Drawing.Font("AppleSDGothicNeoB00", 10F);
+            this.button_gudi2.ForeColor = System.Drawing.Color.White;
+            this.button_gudi2.Location = new System.Drawing.Point(150, 16);
+            this.button_gudi2.Name = "button_gudi2";
+            this.button_gudi2.Size = new System.Drawing.Size(80, 33);
+            this.button_gudi2.TabIndex = 5;
+            this.button_gudi2.Text = "저장";
+            this.button_gudi2.UseVisualStyleBackColor = false;
+            // 
+            // button_gudi1
+            // 
+            this.button_gudi1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_gudi1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(142)))), ((int)(((byte)(150)))));
+            this.button_gudi1.ButtonType = FinalProject1_winform.ButtonStyle.Normal;
+            this.button_gudi1.FlatAppearance.BorderSize = 0;
+            this.button_gudi1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_gudi1.Font = new System.Drawing.Font("AppleSDGothicNeoB00", 10F);
+            this.button_gudi1.ForeColor = System.Drawing.Color.White;
+            this.button_gudi1.Location = new System.Drawing.Point(242, 16);
+            this.button_gudi1.Name = "button_gudi1";
+            this.button_gudi1.Size = new System.Drawing.Size(80, 33);
+            this.button_gudi1.TabIndex = 6;
+            this.button_gudi1.Text = "닫기";
+            this.button_gudi1.UseVisualStyleBackColor = false;
+            // 
+            // label_gudi2
+            // 
+            this.label_gudi2.AutoSize = true;
+            this.label_gudi2.Font = new System.Drawing.Font("AppleSDGothicNeoM00", 10F);
+            this.label_gudi2.Location = new System.Drawing.Point(80, 104);
+            this.label_gudi2.Name = "label_gudi2";
+            this.label_gudi2.Size = new System.Drawing.Size(73, 17);
+            this.label_gudi2.TabIndex = 4;
+            this.label_gudi2.Text = "label_gudi2";
+            // 
+            // label_gudi3
+            // 
+            this.label_gudi3.AutoSize = true;
+            this.label_gudi3.Font = new System.Drawing.Font("AppleSDGothicNeoM00", 10F);
+            this.label_gudi3.Location = new System.Drawing.Point(79, 150);
+            this.label_gudi3.Name = "label_gudi3";
+            this.label_gudi3.Size = new System.Drawing.Size(74, 17);
+            this.label_gudi3.TabIndex = 5;
+            this.label_gudi3.Text = "label_gudi3";
+            // 
+            // label_gudi4
+            // 
+            this.label_gudi4.AutoSize = true;
+            this.label_gudi4.Font = new System.Drawing.Font("AppleSDGothicNeoM00", 10F);
+            this.label_gudi4.Location = new System.Drawing.Point(79, 196);
+            this.label_gudi4.Name = "label_gudi4";
+            this.label_gudi4.Size = new System.Drawing.Size(74, 17);
+            this.label_gudi4.TabIndex = 6;
+            this.label_gudi4.Text = "label_gudi4";
+            // 
+            // placeholderNew1
+            // 
+            this.placeholderNew1.Font = new System.Drawing.Font("AppleSDGothicNeoM00", 10F);
+            this.placeholderNew1.InputType = FinalProject1_winform.validType.Common;
+            this.placeholderNew1.Location = new System.Drawing.Point(242, 101);
+            this.placeholderNew1.Name = "placeholderNew1";
+            this.placeholderNew1.PlaceHolderActiveForeColor = System.Drawing.Color.Gray;
+            this.placeholderNew1.PlaceHolderFont = new System.Drawing.Font("AppleSDGothicNeoM00", 10F);
+            this.placeholderNew1.PlaceHolderForeColor = System.Drawing.Color.DarkGray;
+            this.placeholderNew1.PlaceHolderText = "필수 입력 사항입니다.";
+            this.placeholderNew1.Size = new System.Drawing.Size(149, 25);
+            this.placeholderNew1.TabIndex = 7;
             // 
             // ProcessInsert
             // 
@@ -84,13 +168,19 @@ namespace FinalProject1_winform.Process
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(474, 386);
+            this.Controls.Add(this.placeholderNew1);
+            this.Controls.Add(this.label_gudi4);
+            this.Controls.Add(this.label_gudi3);
+            this.Controls.Add(this.label_gudi2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "ProcessInsert";
             this.Text = "ProcessInsert";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_gudi1)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -100,5 +190,11 @@ namespace FinalProject1_winform.Process
         private PictureBox_gudi pictureBox_gudi1;
         private Label_gudi label_gudi1;
         private System.Windows.Forms.Panel panel2;
+        private Button_gudi button_gudi2;
+        private Button_gudi button_gudi1;
+        private Label_gudi label_gudi2;
+        private Label_gudi label_gudi3;
+        private Label_gudi label_gudi4;
+        private PlaceholderNew placeholderNew1;
     }
 }
