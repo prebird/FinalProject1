906c652b25ec35c0410af88ef0824dd53294eee4
yilee <nopkda@gmail.com>
2021-06-25 16:15:02 +0900

    첫 동기화

---
+++ FinalProject1_winform/Process/frmMaterialPrice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject1_winform.Process
+{
+    public partial class frmMaterialPrice : FinalProject1_winform.Basic3
+    {
+        public frmMaterialPrice()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
