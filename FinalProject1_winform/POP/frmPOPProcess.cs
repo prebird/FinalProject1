c5f42e1ba2b3d21b41c19ee06ef4cc67f399de0a
nopkda@gmail.com <nopkda@gmail.com>
2021-06-28 17:34:42 +0900

    .p

---
+++ FinalProject1_winform/POP/frmPOPProcess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject1_winform.POP
+{
+    public partial class frmPOPProcess : Form
+    {
+        public frmPOPProcess()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPOPProcess_Load(object sender, EventArgs e)
+        {
+            progressBar1.Value = 50;
+        }
+    }
+}
