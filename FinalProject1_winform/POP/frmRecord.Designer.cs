ad12792a1b81b839f8526e162e0a548a2b40291d
nopkda@gmail.com <nopkda@gmail.com>
2021-06-29 00:07:08 +0900

    .

---
+++ FinalProject1_winform/POP/frmRecord.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace FinalProject1_winform.POP
+{
+    partial class frmRecord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRecord));
+            this.button_gudi1 = new FinalProject1_winform.Button_gudi();
+            this.button_gudi5 = new FinalProject1_winform.Button_gudi();
+            this.button_gudi3 = new FinalProject1_winform.Button_gudi();
+            this.label_gudi3 = new FinalProject1_winform.Label_gudi();
+            this.label_gudi2 = new FinalProject1_winform.Label_gudi();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox_gudi1 = new FinalProject1_winform.PictureBox_gudi();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox_gudi2 = new FinalProject1_winform.PictureBox_gudi();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ucDateTimePicker1 = new FinalProject1_winform.UcDateTimePicker();
+            this.button_gudi8 = new FinalProject1_winform.Button_gudi();
+            this.label_gudi1 = new FinalProject1_winform.Label_gudi();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_gudi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_gudi2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_gudi1
+            // 
+            this.button_gudi1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(191)))), ((int)(((byte)(201)))));
+            this.button_gudi1.ButtonType = FinalProject1_winform.ButtonStyle.Middle;
+            this.button_gudi1.FlatAppearance.BorderSize = 0;
+            this.button_gudi1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_gudi1.Font = new System.Drawing.Font("AppleSDGothicNeoB00", 10F);
+            this.button_gudi1.ForeColor = System.Drawing.Color.Black;
+            this.button_gudi1.Image = ((System.Drawing.Image)(resources.GetObject("button_gudi1.Image")));
+            this.button_gudi1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_gudi1.Location = new System.Drawing.Point(984, 311);
+            this.button_gudi1.Name = "button_gudi1";
+            this.button_gudi1.Size = new System.Drawing.Size(85, 25);
+            this.button_gudi1.TabIndex = 26;
+            this.button_gudi1.Text = "제품 출하";
+            this.button_gudi1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_gudi1.UseVisualStyleBackColor = false;
+            // 
+            // button_gudi5
+            // 
+            this.button_gudi5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(191)))), ((int)(((byte)(201)))));
+            this.button_gudi5.ButtonType = FinalProject1_winform.ButtonStyle.Middle;
+            this.button_gudi5.FlatAppearance.BorderSize = 0;
+            this.button_gudi5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_gudi5.Font = new System.Drawing.Font("AppleSDGothicNeoB00", 10F);
+            this.button_gudi5.ForeColor = System.Drawing.Color.Black;
+            this.button_gudi5.Image = ((System.Drawing.Image)(resources.GetObject("button_gudi5.Image")));
+            this.button_gudi5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_gudi5.Location = new System.Drawing.Point(1009, 75);
+            this.button_gudi5.Name = "button_gudi5";
+            this.button_gudi5.Size = new System.Drawing.Size(60, 25);
+            this.button_gudi5.TabIndex = 25;
+            this.button_gudi5.Text = "엑셀";
+            this.button_gudi5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_gudi5.UseVisualStyleBackColor = false;
+            // 
+            // button_gudi3
+            // 
+            this.button_gudi3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(191)))), ((int)(((byte)(201)))));
+            this.button_gudi3.ButtonType = FinalProject1_winform.ButtonStyle.Middle;
+            this.button_gudi3.FlatAppearance.BorderSize = 0;
+            this.button_gudi3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_gudi3.Font = new System.Drawing.Font("AppleSDGothicNeoB00", 10F);
+            this.button_gudi3.ForeColor = System.Drawing.Color.Black;
+            this.button_gudi3.Image = ((System.Drawing.Image)(resources.GetObject("button_gudi3.Image")));
+            this.button_gudi3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_gudi3.Location = new System.Drawing.Point(918, 75);
+            this.button_gudi3.Name = "button_gudi3";
+            this.button_gudi3.Size = new System.Drawing.Size(85, 25);
+            this.button_gudi3.TabIndex = 24;
+            this.button_gudi3.Text = "출하 대기";
+            this.button_gudi3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_gudi3.UseVisualStyleBackColor = false;
+            // 
+            // label_gudi3
+            // 
+            this.label_gudi3.AutoSize = true;
+            this.label_gudi3.Font = new System.Drawing.Font("AppleSDGothicNeoB00", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_gudi3.Location = new System.Drawing.Point(43, 320);
+            this.label_gudi3.Name = "label_gudi3";
+            this.label_gudi3.Size = new System.Drawing.Size(77, 16);
+            this.label_gudi3.TabIndex = 23;
+            this.label_gudi3.Text = "출하대기 제품";
+            // 
+            // label_gudi2
+            // 
+            this.label_gudi2.AutoSize = true;
+            this.label_gudi2.Font = new System.Drawing.Font("AppleSDGothicNeoB00", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_gudi2.Location = new System.Drawing.Point(43, 83);
+            this.label_gudi2.Name = "label_gudi2";
+            this.label_gudi2.Size = new System.Drawing.Size(77, 16);
+            this.label_gudi2.TabIndex = 20;
+            this.label_gudi2.Text = "작업완료 제품";
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Location = new System.Drawing.Point(12, 342);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1057, 223);
+            this.panel3.TabIndex = 22;
+            // 
+            // pictureBox_gudi1
+            // 
+            this.pictureBox_gudi1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_gudi1.Image")));
+            this.pictureBox_gudi1.Location = new System.Drawing.Point(12, 317);
+            this.pictureBox_gudi1.Name = "pictureBox_gudi1";
+            this.pictureBox_gudi1.Size = new System.Drawing.Size(25, 19);
+            this.pictureBox_gudi1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_gudi1.TabIndex = 21;
+            this.pictureBox_gudi1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Location = new System.Drawing.Point(12, 106);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1057, 196);
+            this.panel2.TabIndex = 19;
+            // 
+            // pictureBox_gudi2
+            // 
+            this.pictureBox_gudi2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_gudi2.Image")));
+            this.pictureBox_gudi2.Location = new System.Drawing.Point(12, 81);
+            this.pictureBox_gudi2.Name = "pictureBox_gudi2";
+            this.pictureBox_gudi2.Size = new System.Drawing.Size(25, 19);
+            this.pictureBox_gudi2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox_gudi2.TabIndex = 18;
+            this.pictureBox_gudi2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
+            this.panel1.Controls.Add(this.ucDateTimePicker1);
+            this.panel1.Controls.Add(this.button_gudi8);
+            this.panel1.Controls.Add(this.label_gudi1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1057, 50);
+            this.panel1.TabIndex = 17;
+            // 
+            // ucDateTimePicker1
+            // 
+            this.ucDateTimePicker1.FromDate = new System.DateTime(2021, 6, 28, 23, 34, 13, 29);
+            this.ucDateTimePicker1.Location = new System.Drawing.Point(73, 8);
+            this.ucDateTimePicker1.Name = "ucDateTimePicker1";
+            this.ucDateTimePicker1.Size = new System.Drawing.Size(257, 30);
+            this.ucDateTimePicker1.TabIndex = 10;
+            this.ucDateTimePicker1.ToDate = new System.DateTime(2021, 6, 28, 23, 34, 13, 19);
+            // 
+            // button_gudi8
+            // 
+            this.button_gudi8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(146)))), ((int)(((byte)(209)))));
+            this.button_gudi8.ButtonType = FinalProject1_winform.ButtonStyle.Highlight;
+            this.button_gudi8.FlatAppearance.BorderSize = 0;
+            this.button_gudi8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_gudi8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button_gudi8.ForeColor = System.Drawing.Color.White;
+            this.button_gudi8.Image = ((System.Drawing.Image)(resources.GetObject("button_gudi8.Image")));
+            this.button_gudi8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_gudi8.Location = new System.Drawing.Point(959, 11);
+            this.button_gudi8.Name = "button_gudi8";
+            this.button_gudi8.Size = new System.Drawing.Size(78, 25);
+            this.button_gudi8.TabIndex = 9;
+            this.button_gudi8.Text = "조회";
+            this.button_gudi8.UseVisualStyleBackColor = false;
+            // 
+            // label_gudi1
+            // 
+            this.label_gudi1.AutoSize = true;
+            this.label_gudi1.Font = new System.Drawing.Font("AppleSDGothicNeoB00", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_gudi1.Location = new System.Drawing.Point(12, 15);
+            this.label_gudi1.Name = "label_gudi1";
+            this.label_gudi1.Size = new System.Drawing.Size(55, 16);
+            this.label_gudi1.TabIndex = 0;
+            this.label_gudi1.Text = "■ 지시일";
+            // 
+            // frmRecord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 620);
+            this.Controls.Add(this.button_gudi1);
+            this.Controls.Add(this.button_gudi5);
+            this.Controls.Add(this.button_gudi3);
+            this.Controls.Add(this.label_gudi3);
+            this.Controls.Add(this.label_gudi2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.pictureBox_gudi1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pictureBox_gudi2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmRecord";
+            this.Text = "frmRecord";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_gudi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_gudi2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button_gudi button_gudi1;
+        private Button_gudi button_gudi5;
+        private Button_gudi button_gudi3;
+        private Label_gudi label_gudi3;
+        private Label_gudi label_gudi2;
+        private System.Windows.Forms.Panel panel3;
+        private PictureBox_gudi pictureBox_gudi1;
+        private System.Windows.Forms.Panel panel2;
+        private PictureBox_gudi pictureBox_gudi2;
+        private System.Windows.Forms.Panel panel1;
+        private UcDateTimePicker ucDateTimePicker1;
+        private Button_gudi button_gudi8;
+        private Label_gudi label_gudi1;
+    }
+}
