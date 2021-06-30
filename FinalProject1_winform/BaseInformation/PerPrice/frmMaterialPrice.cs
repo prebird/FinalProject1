00ad9254ed1b0af2d4a5ac5bcf567d3a311cb90b
yilee <nopkda@gmail.com>
2021-06-30 16:38:56 +0900

    ..

--- FinalProject1_winform/BaseInformation/PerPrice/frmMaterialPrice.cs
+++ FinalProject1_winform/BaseInformation/PerPrice/frmMaterialPrice.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FinalProject1_winform.Process
+namespace FinalProject1_winform
 {
     public partial class frmMaterialPrice : Basic3
     {
