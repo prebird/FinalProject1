665a861f29c5700594451deb70cade8ea1cfbc59
PARK JS <qkrwotjdvvv@naver.com>
2021-07-01 18:11:38 +0900

    재성

--- FinalProject1_winform/Service/ItemService.cs
+++ FinalProject1_winform/Service/ItemService.cs
@@ -10,6 +10,54 @@ namespace FinalProject1_winform
 {
     class ItemService
     {
+        // 전체 조회
+        public List<ItemVO> GetAllCompany()
+        {
+            ItemDAC dac = new ItemDAC();
+            List<ItemVO> list = dac.GetAllItem();
+            dac.Dispose();
+
+            return list;
+        }
+
+        // 특정 조회 
+        public ItemVO GetOneItem(int itemID)
+        {
+            ItemDAC dac = new ItemDAC();
+            ItemVO cmp = dac.GetOneItem(itemID);
+            dac.Dispose();
+
+            return cmp;
+        }
+
+        // 부분 조회
+        public List<ItemVO> GetPartialItem(string itemType, string itemName, string inHouse, string outHouse, char yn) // 부분 인수
+        {
+            ItemDAC dac = new ItemDAC();
+            List<ItemVO> list = dac.GetPartialItem(itemType, itemName, inHouse, outHouse, yn); // 부분 인수
+            dac.Dispose();
+
+            return list;
+        }
+
+        // 등록, 수정
+        public bool InsertUpdateItem(ItemVO item)
+        {
+            ItemDAC dac = new ItemDAC();
+            bool result = dac.InsertUpdateItem(item);
+            dac.Dispose();
+
+            return result;
+        }
+
+        // 삭제
+        public bool Deleteitem(int itemID)
+        {
+            ItemDAC dac = new ItemDAC();
+            bool result = dac.DeleteItem(itemID);
+            dac.Dispose();
+            return result;
+        }
 
 
     }
