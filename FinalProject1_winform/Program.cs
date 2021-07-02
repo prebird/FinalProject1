d0d9c9821c1cb73fc6ccf2a91f6e34295c0cab2b
정용규 <iopengom@naver.com>
2021-07-02 14:24:55 +0900

    dd

--- FinalProject1_winform/Program.cs
+++ FinalProject1_winform/Program.cs
@@ -17,7 +17,7 @@ namespace FinalProject1_winform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmCompanyInfo());
         }
     }
 }
