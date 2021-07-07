d87590732066eda4fe3e4673b809fd410a51efe8
PARK JS <qkrwotjdvvv@naver.com>
2021-07-07 23:42:38 +0900

    재성

--- FinalProject1_winform/BOM/frmBOMList.cs
+++ FinalProject1_winform/BOM/frmBOMList.cs
@@ -23,8 +23,8 @@ namespace FinalProject1_winform
             CommonUtil.SetInitGridView(dgv_Item);
             CommonUtil.AddGridTextColumn(dgv_Item, "품목번호", "Item_ID", DataGridViewContentAlignment.MiddleCenter, colWidth: 85);
             CommonUtil.AddGridTextColumn(dgv_Item, "품목유형", "Item_Category", DataGridViewContentAlignment.MiddleCenter);
-            CommonUtil.AddGridTextColumn(dgv_Item, "품목코드", "Item_Code", DataGridViewContentAlignment.MiddleCenter);
-            CommonUtil.AddGridTextColumn(dgv_Item, "품목명", "Item_Name", DataGridViewContentAlignment.MiddleCenter);
+            CommonUtil.AddGridTextColumn(dgv_Item, "품목코드", "Item_Code", DataGridViewContentAlignment.MiddleCenter, colWidth: 150);
+            CommonUtil.AddGridTextColumn(dgv_Item, "품목명", "Item_Name", DataGridViewContentAlignment.MiddleCenter, colWidth: 150);
             CommonUtil.AddGridTextColumn(dgv_Item, "단위수량", "Item_UnitQTY", DataGridViewContentAlignment.MiddleCenter, colWidth: 85);
             CommonUtil.AddGridTextColumn(dgv_Item, "측정방식", "Item_CheckType", DataGridViewContentAlignment.MiddleCenter, colWidth: 85, visibility: false);
             CommonUtil.AddGridTextColumn(dgv_Item, "발주방식", "Item_OrderType", DataGridViewContentAlignment.MiddleCenter, colWidth: 85, visibility: false);
@@ -39,11 +39,11 @@ namespace FinalProject1_winform
 
             // 정전개 바인딩
             CommonUtil.SetInitGridView(dgv_Forward);
-            CommonUtil.AddGridTextColumn(dgv_Forward, "level", "level", colWidth: 50);
-            CommonUtil.AddGridTextColumn(dgv_Forward, "INFO", "INFO", colWidth: 240);
-            CommonUtil.AddGridTextColumn(dgv_Forward, "자품목ID", "BOM_JaItemID", colWidth: 80);
-            CommonUtil.AddGridTextColumn(dgv_Forward, "소요량", "BOM_UseQTY", colWidth: 70);
-            CommonUtil.AddGridTextColumn(dgv_Forward, "모품목ID", "BOM_MoItemID", colWidth: 80,visibility:false);
+            CommonUtil.AddGridTextColumn(dgv_Forward, "level", "level", DataGridViewContentAlignment.MiddleCenter, colWidth: 50);
+            CommonUtil.AddGridTextColumn(dgv_Forward, "INFO", "INFO", colWidth: 200);
+            CommonUtil.AddGridTextColumn(dgv_Forward, "자품목ID", "BOM_JaItemID", DataGridViewContentAlignment.MiddleCenter, colWidth: 80);
+            CommonUtil.AddGridTextColumn(dgv_Forward, "소요량", "BOM_UseQTY", DataGridViewContentAlignment.MiddleCenter, colWidth: 70);
+            CommonUtil.AddGridTextColumn(dgv_Forward, "모품목ID", "BOM_MoItemID", DataGridViewContentAlignment.MiddleCenter, colWidth: 80,visibility:false);
             //CommonUtil.AddGridTextColumn(dgv_Forward, "소요계획여부", "BOM_DemandYN", colWidth: 80);
             //CommonUtil.AddGridTextColumn(dgv_Forward, "사용여부", "BOM_YN", colWidth: 80);
             //CommonUtil.AddGridTextColumn(dgv_Forward, "비고", "BOM_Content", colWidth: 80);
@@ -51,7 +51,7 @@ namespace FinalProject1_winform
             // 역전개 바인딩
             CommonUtil.SetInitGridView(dgv_Reverse);
             CommonUtil.AddGridTextColumn(dgv_Reverse, "level", "level", colWidth: 50);
-            CommonUtil.AddGridTextColumn(dgv_Reverse, "모품목명", "Item_Name", colWidth: 240);
+            CommonUtil.AddGridTextColumn(dgv_Reverse, "모품목명", "Item_Name", colWidth: 200);
             CommonUtil.AddGridTextColumn(dgv_Reverse, "모품목ID", "BOM_MoItemID", colWidth: 90);
             CommonUtil.AddGridTextColumn(dgv_Reverse, "소요량", "BOM_UseQTY", colWidth: 70);
             CommonUtil.AddGridTextColumn(dgv_Reverse, "자품목ID", "BOM_JaItemID", colWidth: 90);
@@ -75,9 +75,9 @@ namespace FinalProject1_winform
             dgv_Forward.DataSource = null;
             dgv_Forward.DataSource = service1.BOMForward(itemID);
 
-            //BOMService service2 = new BOMService();
-            //dgv_Reverse.DataSource = null;
-            //dgv_Reverse.DataSource = service2.BOMReverse(itemID);
+            BOMService service2 = new BOMService();
+            dgv_Reverse.DataSource = null;
+            dgv_Reverse.DataSource = service2.BOMReverse(itemID);
         }
 
         private void btn_Insert_Click(object sender, EventArgs e)
--- FinalProject1_winform/Program.cs
+++ FinalProject1_winform/Program.cs
@@ -17,7 +17,7 @@ namespace FinalProject1_winform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmBORInsert());
+            Application.Run(new frmBOMList());
         }
     }
 }
