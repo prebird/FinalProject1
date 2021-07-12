52fa85ab3636c651f0b2f33f0b55917a61da699c
nopkda@gmail.com <nopkda@gmail.com>
2021-07-13 03:36:37 +0900

    ..

--- FinalProject1_winform/Program.cs
+++ FinalProject1_winform/Program.cs
@@ -17,7 +17,7 @@ namespace FinalProject1_winform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPPriceIns());
+            Application.Run(new frmCommonCode());
         }
     }
 }
