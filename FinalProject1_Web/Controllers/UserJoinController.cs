d1428eff3b6c11b3e1d073c86bc0f2d42212b235
yonggyujeong <yonggyujeong10@gmail.com>
2021-07-12 14:42:28 +0900

    ㄹ

---
+++ FinalProject1_Web/Controllers/UserJoinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject1_Web.Controllers
+{
+    public class UserJoinController : Controller
+    {
+        // GET: UserJoin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
